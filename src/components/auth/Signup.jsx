772d65b5ca26f81253f4352c330140e28724a950
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-27 14:47:00 +0300

    🚀 Implemented user signup functionality. 📧 Added email validation in the signup form. 🔒 Enhanced password input security. 🎨 Styled the signup page for better user experience. 🔄 Refactored and optimized the signup component code.

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSignupMutation } from '../../services/authQueries';
 
 const Signup = () => {
+
+
+
+    const signupMutation = useSignupMutation();
+
+    const handleSignup = async (formData) => {
+      try {
+        const { data } = await signupMutation.mutateAsync(formData);
+        console.log(data); // Handle success, e.g., redirect to dashboard
+      } catch (error) {
+        console.error(error); // Handle error, e.g., show error message
+      }
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const formData = {
+        name: e.target.elements.name.value,
+        email: e.target.elements.email.value,
+        password: e.target.elements.password.value,
+      };
+      handleSignup(formData);
+    };
+
+
+
+
     return (
         <>
             <div>
@@ -19,17 +47,36 @@ const Signup = () => {
                                 <div className="lg:text-left text-center">
                                     <div className="flex items-center justify-center">
                                         <div className="bg-black flex flex-col w-full border border-gray-900 rounded-lg px-8 py-10">
-                                            <form className="flex flex-col  ">
-                                                <label className="font-bold text-lg text-white">Name</label>
-                                                <input type="text" placeholder="Write Your Name.." className="border rounded-lg py-3 px-3 mt-4 bg-black border-indigo-600 placeholder-white-500 text-white" />
-                                                <label className="font-bold text-lg text-white mt-3">Email</label>
-                                                <input type="email" placeholder="Write Your Email.." className="border rounded-lg py-3 px-3 bg-black border-indigo-600 placeholder-white-500 text-white mb-3" />
-                                                <label className="font-bold text-lg text-white">Password</label>
-                                                <input type="text" placeholder="Write Your Password.." className="border rounded-lg py-3 px-3 mt-4 bg-black border-indigo-600 placeholder-white-500 text-white" />
-                                                <button className="border mt-12 border-indigo-600 bg-black text-white rounded-lg py-3 font-semibold">
-                                                    Signup
+                                            <form onSubmit={handleSubmit} className="flex flex-col  ">
+                                                <label htmlFor="name" className="font-bold text-lg text-white">Name</label>
+                                                <input 
+                                                type="text"
+                                                id="name"
+                                                name="name" 
+                                                placeholder="Write Your Name.." 
+                                                className="border rounded-lg py-3 px-3 mt-4 bg-black border-indigo-600 placeholder-white-500 text-white" />
+                                                <label htmlFor="email" className="font-bold text-lg text-white mt-3">Email</label>
+                                                <input 
+                                               type="email"
+                                               id="email"
+                                               name="email"
+                                                placeholder="Write Your Email.." 
+                                                className="border rounded-lg py-3 px-3 bg-black border-indigo-600 placeholder-white-500 text-white mb-3" />
+                                                <label htmlFor="password" className="font-bold text-lg text-white">Password</label>
+                                                <input 
+                                               type="password"
+                                               id="password"
+                                               name="password"
+                                                placeholder="Write Your Password.." 
+                                                className="border rounded-lg py-3 px-3 mt-4 bg-black border-indigo-600 placeholder-white-500 text-white" />
+                                                <button 
+                                                 type="submit"
+                                                className="border mt-12 border-indigo-600 bg-black text-white rounded-lg py-3 font-semibold"
+                                                disabled={signupMutation.isLoading}
+                                                >
+                                                    {signupMutation.isLoading ? 'Signing up...' : 'Sign Up'}
                                                 </button>
-                                                <div class="flex items-center justify-between mt-4"><span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span> <Link to="/" class="text-xs  text-gray-500 uppercase dark:text-gray-400 hover:underline">Create an account</Link> <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span></div>
+                                                <div class="flex items-center justify-between mt-4"><span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span> <Link to="/signup" class="text-xs  text-gray-500 uppercase dark:text-gray-400 hover:underline">Create an account</Link> <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span></div>
                                             </form>
                                         </div>
                                     </div>
