7670b10cef2533cb8d2a48f98a65ff06f9f5b09b
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-30 03:38:34 +0300

    🚀 Implemented login functionality with email and password. 🔐 Validated form data using login schema for enhanced security. 🌐 Integrated React Router for smooth navigation in the application. 📣 Added success toast notification for a welcoming user experience. 👁️‍🗨️ Toggle password visibility feature for user convenience. 🎨 Styled and organized the login form for a polished appearance. 🔄 Refactored code structure in the Login component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
-import Table from './components/Table/Table';
+import { Table } from '@chakra-ui/react';
+
+
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/table" element={<Table />} />
+        <Route path="/data-table" element={<Table />} />
       </Routes>
     </Router>
   );
